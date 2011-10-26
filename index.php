5c68669cbdfd94ae23abca6056d7ecff6e20a0ba
Jaseem Abid <jaseemabid@gmail.com>
2011-10-26 19:23:35 +0530

    Minimised twitter widget code

--- index.php
+++ index.php
@@ -75,34 +75,7 @@ Hailing from God's own country,teamunwired is an ensemble of engineers-to-be at 
 	<div id="coloumn" style="padding:0 15px; height:355px;">
 		<script src="http://widgets.twimg.com/j/2/widget.js"></script>
 		<script>
-		new TWTR.Widget({
-			version: 2,
-			type: 'profile',
-			rpp: 4,
-			interval: 30000,
-			width: 280,
-			height: 270,
-			theme: {
-			shell: {
-				background: 'rgba(12, 12, 12, 0.6);',
-				color: '#ffffff'
-			},
-			tweets: {
-				background: '#000000',
-				color: '#ffffff',
-				links: '#f31414'
-			}
-			},
-			features: {
-			scrollbar: false,
-			loop: true,
-			live: true,
-			hashtags: true,
-			timestamp: true,
-			avatars: false,
-			behavior: 'all'
-			}
-		}).render().setUser('teamunwired').start();
+		(new TWTR.Widget({version:2,type:"profile",rpp:4,interval:3E4,width:280,height:270,theme:{shell:{background:"rgba(12, 12, 12, 0.6);",color:"#ffffff"},tweets:{background:"#000000",color:"#ffffff",links:"#f31414"}},features:{scrollbar:false,loop:true,live:true,hashtags:true,timestamp:true,avatars:false,behavior:"all"}})).render().setUser("teamunwired").start();
 		</script>
 	</div>
 
