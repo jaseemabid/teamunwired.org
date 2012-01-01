83ee7bd821ffb63c272815fe2dd63b815c129eb6
Jaseem Abid <jaseemabid@gmail.com>
2012-01-02 04:27:38 +0530

    Bugfix : Deck called only on required pages.

--- js/tuw.js
+++ js/tuw.js
@@ -59,13 +59,15 @@ $(document).ready(function(){
 		}, 3000 );
 	} slideshow();
 
-	// Deck initialization
-	$.deck('.slide');
-	$('#style-themes').change(function() {
-		$('#style-theme-link').attr('href', $(this).val());
-	});
-	$('#transition-themes').change(function() {
-		$('#transition-theme-link').attr('href', $(this).val());
-	});
+	if($.deck) {
+		// Deck initialization
+		$.deck('.slide');
+		$('#style-themes').change(function() {
+			$('#style-theme-link').attr('href', $(this).val());
+		});
+		$('#transition-themes').change(function() {
+			$('#transition-theme-link').attr('href', $(this).val());
+		});
+	}
 
 });
