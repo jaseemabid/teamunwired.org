ab7d541013425314c79c900c4f7819ef4e67e8ab
Jaseem Abid <jaseemabid@gmail.com>
2012-05-23 01:43:58 +0530

    Fresh root page

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <title>Team unwired</title>
 <meta charset=UTF-8>
 <link rel="shortcut icon" href="graphics/favicon.ico">
-<link href="css/tuw.css" rel="stylesheet"/>
-<link href="css/polaroid.css" rel="stylesheet"/>
+<link href="css/bootstrap.min.css" rel="stylesheet"/>
+<link href="css/style.min.css" rel="stylesheet"/>
 <meta name="description" content="teamunwired NIT Calicut" />
 <meta name="keywords" content="teamunwired, unwired, nitc, nitcalicut, NIT Calcut, engineering, aero design, BAJA, rocketry " />
 <meta name="author" content="Jaseem Abid" />
@@ -17,101 +17,12 @@
 <meta property="og:image" content="" />
 <meta property="og:site_name" content="teamunwired" />
 <meta property="fb:admins" content="798947421" />
-
 </head>
 <body>
 
-<!-- logos -->
-<div id='logos'>
-	<a href='/'><img src="graphics/tuwlogo100px.png" alt="club logo" title="Teamunwired" class="mainlogo" /></a>
-	<a href="http://www.nitc.ac.in"><img src="graphics/NITC-logo.png" alt="NIT Calcut" title="Visit NITC website" class="mainlogo" /></a>
-	<a href="http://sae.org/"><img src="graphics/sae100px.png" alt="SAE logo" title="SAE International" class="mainlogo" style="float:right; height:90px; position:relative; top:10px;" /></a>
-	<a href="http://bajasaeindia.org/"><img src="graphics/saeindia.png" alt="BAJA SAE India" title="BAJA SAE India" class="mainlogo" style="float:right; height:90px; position:relative; top:10px;" /></a>
-</div>
-<!-- \logos -->
-<!-- global nav-->
-<nav>
-	<ul id="globalnav">
-	<li id="home"><a href="#" class="live">Home</a></li>
-	<li id="baja"><a href="#">Baja SAE</a></li>
-	<li id="activities"><a href="#">Activities</a></li>
-	<li id="gallery"><a href="#">Gallery</a></li>
-	<li id="sponsorship"><a href="#">Sponsorship</a></li>
-	<li id="sponsors"><a href="#">Sponsors</a></li>
-	<li id="team"><a href="#">The Team</a></li>
-	<li id="contact"><a href="#">Contact</a></li>
-	</ul>
-</nav>
-<!-- \ global nav-->
 
-<!-- main display -->
-<div class='maincontent'>
 
-<!-- tab 0--> <!-- Home -->
-<!-- tab 1--> <!-- Baja SAE -->
-<!-- tab 2--> <!-- Activities -->
-<!-- tab 3--> <!-- Gallery -->
-<!-- tab 4--> <!-- Sponsorship -->
-<!-- tab 5--> <!-- Team -->
-<!-- tab 6--> <!-- Contact -->
-<!-- Preloading with tab 0 -->
-
-<div class="main1">
-	<div class="mainTitle" style="top: 25px; left: 250px; border-radius:3px;" >
-		<span style="color:#c00002">UN</span>INTRODUCTION
-	</div>
-	<div class="text" style="width:460px;top:95px; left:150px;">
-		You can't do anything about the length of your life, but you can do something about its width and depth.
-	</div>
-	<img src='graphics/homepan.png' class="goldenImage" alt="Car at hockey ground"/>
-</div>
-<div class='main2'>
-	<div class='boxtitle'>About teamunwired</div>
-Hailing from God's own country,teamunwired is an ensemble of engineers-to-be at National Institute of Technology Calicut (NIT Calicut), one of the country's best technical institutes. We call ourselves Unwired, a team that relishes on engineering challenges. The primary motivation for competing in Baja SAE lies in the desire to go through the hands-on experience of building an engineering prototype and the thrill in watching it work. The 25-membered team comprises inter disciplinary undergraduate students from 2nd to 4th year.<br/><br/>
-<a target="_blank" href='content/TeamUnwiredbrochure.pdf' >Download Marketing Brochure</a>
-</div>
-</div>
-
-<!--\	main display -->
-<div id="coloumns">
-
-	<div class=coloumn style="padding:0 15px; height:355px;">
-		<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-		<script>
-		(new TWTR.Widget({version:2,type:"profile",rpp:4,interval:3E4,width:280,height:270,theme:{shell:{background:"rgba(12, 12, 12, 0.6);",color:"#ffffff"},tweets:{background:"#000000",color:"#ffffff",links:"#f31414"}},features:{scrollbar:false,loop:true,live:true,hashtags:true,timestamp:true,avatars:false,behavior:"all"}})).render().setUser("teamunwired").start();
-		</script>
-	</div>
-
-	<div class=coloumn	style="margin:0px 10px">
-		<div class='boxtitle' style="height:300px" id='SponsorTitle' >Current Sponsor</div>
-		<br />
-		<a class='SponsorLink' href='http://www.solidworks.com/'>
-			<img id='jSlider' src="graphics/logos/sw.png" class="sponLogos" alt="Solid works"/>
-		</a>
-	</div>
-
-	<div class=coloumn>
-		<div class='boxtitle'>News and updates</div>
-		<ul class='news'>
-			<li>The new car : gasotech unwind 2.0. Thanks to our title sponsor Gasotech</li>
-			<li><a target="_blank" href='content/TeamUnwiredbrochure.pdf' >Download Marketing Brochure</a></li>
-			<li><a target="_blank" href='http://meandtuw.blogspot.com/'>Few words from the team captain</a> </li>
-			<li>Teamunwired is the only team selected from kerala to participate in BAJA SAE India 2012.</li>
-			<li>Designing process completed and virtual tests ok. Fabrication and marketing in active phase.</li>
-			<li>Teamunwired orientation was a huge success. First ever club orientation at NITC OAT. More info soon</li>
-		</ul>
-	</div>
-</div>
-
-<div id="bottomlinks">
-	<ul>
-		<li>(c) www.teamunwired.org</li>
-	</ul>
-</div>
-
-<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
-<script type="text/javascript" src="js/jquery.accordion.js"></script>
-<script type="text/javascript" src="js/tuw.js"></script>
+<script type="text/javascript" src="js/teamunwired.min.js"></script>
 </body>
 </html>
 
